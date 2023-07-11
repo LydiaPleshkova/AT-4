bfea8c829251a68cd833885b29aa632d082d4489
Lydia Pleshkova <lydia.y.pleshkova@gmail.com>
2023-07-11 05:25:21 +0300

    build gradle updated

---
+++ src/test/java/ru/netology/selenide/AppCardDeliveryTest.java
@@ -0,0 +1,13 @@
+package ru.netology.selenide;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppCardDeliveryTest {
+    private String generateDate(addDays,
+
+}
