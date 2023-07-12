e0ea18dc4b2a215d5204050b5c6dff9e8c84d426
Lydia Pleshkova <lydia.y.pleshkova@gmail.com>
2023-07-12 06:21:53 +0300

    test updated

--- src/test/java/ru/netology/selenide/appCardDeliveryTest.java
+++ src/test/java/ru/netology/selenide/appCardDeliveryTest.java
@@ -20,15 +20,15 @@ public class appCardDeliveryTest {
     public void shouldBeSuccessfulCompleted() {
         open("http://0.0.0.0:9999");
         $("[data-test-id='city'] input").setValue("Рязань");
-        String futureDate = generateDate(4, "dd.MM.yyyy");
+        String currentDate = generateDate(4, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id='date'] input").sendKeys("12.07.2023");
+        $("[data-test-id='date'] input").sendKeys(currentDate);
         $("[data-test-id='name'] input").setValue("Иванова Ольга");
         $("[data-test-id='phone'] input").setValue("+71234567890");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
         $(".notification__content")
-                .shouldBe(Condition.visible, Duration.ofSeconds(14))
-                .shouldHave(Condition.exactText("Успешно! Встреча успешно зарегистрирована на " + futureDate));
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Успешно! Встреча успешно забронирована на " + currentDate));
     }
 }
